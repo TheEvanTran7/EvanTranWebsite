39ea5e0ce2baa4ae0499faa051c4f40c048769e1
et232 <87101704+TheEvanTran@users.noreply.github.com>
2024-01-26 16:19:09 -0500

    fix

--- script.js
+++ script.js
@@ -1,56 +1,48 @@
-var page1 = document.getElementById('form-p1');
-var page2 = document.getElementById('form-p2');
-var page3 = document.getElementById('form-p3');
+var page1 = document.getElementById("form-p1");
+var page2 = document.getElementById("form-p2");
+var page3 = document.getElementById("form-p3");
 
-var btn = document.querySelector(".page-1-btn")
-console.log(btn)
+var btn = document.querySelector(".page-1-btn");
+console.log(btn);
 
-function toPage1(){
-    console.log("hello");
-    page1.style.display = "block";
-    page2.style.display = "none";
-    page3.style.display = "none";
+function toPage1() {
+  console.log("hello");
+  page1.style.display = "block";
+  page2.style.display = "none";
+  page3.style.display = "none";
 }
 
-function toPage2(){
-    page1.style.display = "none";
-    page2.style.display = "block";
-    page3.style.display = "none";
+function toPage2() {
+  page1.style.display = "none";
+  page2.style.display = "block";
+  page3.style.display = "none";
 }
 
-function toPage3(){
-    page1.style.display = "none";
-    page2.style.display = "none";
-    page3.style.display = "block";
-}
-
-var proj = document.getElementsByClassName("proj-wrapper");
-
-function topFunction() {
-    document.proj.scrollTop = 0;
-    document.documentElement.scrollTop= 0;
+function toPage3() {
+  page1.style.display = "none";
+  page2.style.display = "none";
+  page3.style.display = "block";
 }
 
 var xBtn = document.getElementById("x");
 var modal = document.getElementById("nto");
 
-function openInfo(btnId){
-    var modal = document.querySelectorAll(".modal")
-    modal.forEach((btn) => {
-        btn.style.display = "none";
-    })
-    modal[btnId].style.display = "block"
+function openInfo(btnId) {
+  var modal = document.querySelectorAll(".modal");
+  modal.forEach((btn) => {
+    btn.style.display = "none";
+  });
+  modal[btnId].style.display = "block";
 }
 
-function closeModal(){
-    var modal = document.querySelectorAll(".modal")
-    modal.forEach((btn) => {
-        btn.style.display = "none";
-    })
+function closeModal() {
+  var modal = document.querySelectorAll(".modal");
+  modal.forEach((btn) => {
+    btn.style.display = "none";
+  });
 }
 
 var loader = document.getElementById("preloader");
-window.addEventListener("load", function() {
-    loader.style.display = "none";
-
-})
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
